acd377c5419ef92012824dc5b9dbed899985676a
Taehoon Yoo <noep@naver.com>
2017-10-02 12:43:07 +0900

    1.2 상속을 통한 확장 방법이 제공되는 UserDao

---
+++ toby-01/src/main/java/io/noep/dao/NUserDao.java
@@ -0,0 +1,35 @@
+package io.noep.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Created by Taehoon Yoo
+ * User  : taehoon
+ * Date  : 2017. 10. 2.
+ * Time  : 오후 12:39
+ * Page  : http:noep.github.io
+ * Email : noep@naver.com
+ * Desc  :
+ */
+public class NUserDao extends UserDao {
+
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
+        /**
+         * ref : https://slipp.net/questions/276
+         * Class.forName 을 실행하는 경우 문자열로 전달되는 클래스가 메모리 상에 등록된다.
+         * 'com.mysql.jdbc.Driver 가 메모리에 로드되면서 static 절이 실행된다.
+         * 이후 DriverManager에 드라이버 등록 메소드를 수행하는 것을 알 수 있다
+         */
+
+        /**
+         * 라고 생각했는데, java1.6 이상부터는 서비스로더 기반으로 JDBC Driver가 자동으로 등록된다고 한다
+         * 그래서 해당 코드를 호출하지 않아도 돌아간다.
+         */
+        //Class.forName("com.mysql.jdbc.Driver");
+        Connection c = DriverManager.getConnection(
+                "jdbc:mysql://localhost/toby-test", "root", null);
+        return c;
+    }
+}
--- toby-01/src/main/java/io/noep/dao/UserDao.java
+++ toby-01/src/main/java/io/noep/dao/UserDao.java
@@ -11,14 +11,12 @@ import java.sql.*;
  * Time  : 오후 12:06
  * Page  : http:noep.github.io
  * Email : noep@naver.com
- * Desc  : mysql driver 버전업으로 패키지 구조가 바뀌었나 보다
- */
-public class UserDao {
+ * Desc  : 6점 대 mysql driver를 쓰면 패키지 구조가 바뀐다
+ * */
+public abstract class UserDao {
 
     public void add(User user) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.jdbc.Driver");
-        Connection c = DriverManager.getConnection(
-                "jdbc:mysql://localhost/toby-test", "root", null);
+        Connection c = this.getConnection();
 
         PreparedStatement ps = c.prepareStatement(
                 "insert into users(id,name,password) values (?,?,?)");
@@ -33,9 +31,7 @@ public class UserDao {
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.jdbc.Driver");
-        Connection c = DriverManager.getConnection(
-                "jdbc:mysql://localhost/toby-test", "root", null);
+        Connection c = this.getConnection();
 
         PreparedStatement ps = c.prepareStatement(
                 "select * from users where id = ?");
@@ -55,4 +51,13 @@ public class UserDao {
 
         return user;
     }
+
+    /**
+     * 중복 코드의 메소드 추출
+     * @return
+     * @throws ClassNotFoundException
+     * @throws SQLException
+     */
+    public abstract Connection getConnection() throws ClassNotFoundException, SQLException;
+
 }
