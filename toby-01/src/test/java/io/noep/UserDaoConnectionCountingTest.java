f1de50ffa4863b4054ccad1f3af029a8a050b39f
Taehoon Yoo <noep@naver.com>
2017-10-02 17:11:05 +0900

    1.8 xml을 이용한 의존성 주입

--- toby-01/src/test/java/io/noep/UserDaoConnectionCountingTest.java
+++ toby-01/src/test/java/io/noep/UserDaoConnectionCountingTest.java
@@ -2,9 +2,12 @@ package io.noep;
 
 import io.noep.dao.CountingConnectionMaker;
 import io.noep.dao.CountingDaoFactory;
+import io.noep.dao.DConnectionMaker;
 import io.noep.dao.UserDao;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 import java.sql.SQLException;
 
@@ -19,6 +22,11 @@ import java.sql.SQLException;
  */
 public class UserDaoConnectionCountingTest {
 
+    /**
+     * Java Configuration을 이용한 spring context 설정
+     * @throws SQLException
+     * @throws ClassNotFoundException
+     */
     @Test
     public void test() throws SQLException, ClassNotFoundException {
         AnnotationConfigApplicationContext context =
@@ -31,9 +39,19 @@ public class UserDaoConnectionCountingTest {
         System.out.println("Connection counter : " + ccm.getCounter());
         dao.get("whiteship");
         System.out.println("Connection counter : " + ccm.getCounter());
+    }
 
+    /**
+     * xml을 이용한 spring context 설정
+     * @throws SQLException
+     * @throws ClassNotFoundException
+     */
+    @Test
+    public void test2() throws SQLException, ClassNotFoundException {
+        ApplicationContext context =
+                new GenericXmlApplicationContext("application-context.xml");
 
-
-
+        UserDao dao = context.getBean("userDao", UserDao.class);
+        System.out.println(dao.get("whiteship").toString());
     }
 }
