b85fba600a0980e6341875837bf49b3cf47bd204
Taehoon Yoo <noep@naver.com>
2017-11-14 21:12:38 +0900

    7.2 xsd, 인터페이스 분리와 자기참조 빈 임시 커밋

---
+++ toby-07/src/test/java/io/noep/jaxb/JaxbTest.java
@@ -0,0 +1,47 @@
+package io.noep.jaxb;
+
+import io.noep.SqlType;
+import io.noep.Sqlmap;
+import org.junit.Test;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.IOException;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Created by Taehoon Yoo
+ * User  : taehoon
+ * Date  : 2017. 11. 7.
+ * Time  : 오후 9:27
+ * Page  : http:noep.github.io
+ * Email : noep@naver.com
+ * Desc  :
+ */
+public class JaxbTest {
+
+    @Test
+    public void readSqlmap() throws JAXBException, IOException {
+        String contextPath = Sqlmap.class.getPackage().getName();
+        JAXBContext context = JAXBContext.newInstance(contextPath);
+
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+
+        Sqlmap sqlmap = (Sqlmap) unmarshaller.unmarshal(
+                getClass().getResourceAsStream("sqlmap.xml"));
+
+        List<SqlType> sqlList = sqlmap.getSql();
+
+        assertThat(sqlList.size(), is(3));
+        assertThat(sqlList.get(0).getKey(), is("add"));
+        assertThat(sqlList.get(0).getValue(), is("insert"));
+        assertThat(sqlList.get(1).getKey(), is("get"));
+        assertThat(sqlList.get(1).getValue(), is("select"));
+        assertThat(sqlList.get(2).getKey(), is("delete"));
+        assertThat(sqlList.get(2).getValue(), is("delete"));
+    }
+}
