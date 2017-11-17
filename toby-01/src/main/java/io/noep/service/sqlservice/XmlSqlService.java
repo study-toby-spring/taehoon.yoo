4ece584fe92d8afc215f9449b00057bab5428d59
Taehoon Yoo <noep@naver.com>
2017-11-17 21:22:10 +0900

    7.2.2 XML 파일을 이용하는 SQL 서비스

---
+++ toby-01/src/main/java/io/noep/service/sqlservice/XmlSqlService.java
@@ -0,0 +1,53 @@
+package io.noep.service.sqlservice;
+
+import io.noep.dao.UserDao;
+import io.noep.jaxb.SqlType;
+import io.noep.jaxb.Sqlmap;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Taehoon Yoo
+ * User  : taehoon
+ * Date  : 2017. 11. 17.
+ * Time  : 오후 9:04
+ * Page  : http:noep.github.io
+ * Email : noep@naver.com
+ * Desc  :
+ */
+public class XmlSqlService implements SqlService {
+
+    private Map<String, String> sqlMap = new HashMap<>();
+
+    public XmlSqlService() {
+        String contextPath = Sqlmap.class.getPackage().getName();
+
+        try {
+            JAXBContext context = JAXBContext.newInstance(contextPath);
+            Unmarshaller unmarshaller = context.createUnmarshaller();
+            InputStream is = UserDao.class.getResourceAsStream("sqlmap.xml");
+            Sqlmap sqlmap = (Sqlmap) unmarshaller.unmarshal(is);
+
+            for(SqlType sql : sqlmap.getSql()) {
+                sqlMap.put(sql.getKey(), sql.getValue());
+            }
+
+        } catch (JAXBException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public String getSql(String key) throws SqlRetrievalFailureException {
+        String sql = sqlMap.get(key);
+        if (sql == null)
+            throw new SqlRetrievalFailureException(key + "를 이용해서 SQL을 찾을 수 없습니다.");
+        else
+            return sql;
+    }
+}
